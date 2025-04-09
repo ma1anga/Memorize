1f08a34eb796888c67fe2d0822f597f845ac017e
Pavlo Bilousov <bilousov.p@icloud.com>
2025-04-09 23:49:36 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Pavlo Bilousov on 09/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
