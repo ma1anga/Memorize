c44fa2130f0b7d0f70d960f499b5c899de0678ec
Pavlo Bilousov <bilousov.p@icloud.com>
2025-04-14 22:48:20 +0200

    State after lecture 4

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,10 +7,35 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    var model: MemorizeGame<String>
+
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙", "🙀", "👹", "😱", "☠️", "🍭"]
     
-    init(model: MemorizeGame<String>) {
-        self.model = model
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame<String>(numberOfPairsOfCards: 16) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+    
+    @Published private var model = createMemoryGame()
+    
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
+        objectWillChange.send()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
     }
 }
